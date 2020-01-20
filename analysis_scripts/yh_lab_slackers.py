71c8330c4df63f71e871f7313e546f5e6dce8412
Yoon Hee Choi <yoonheec@student.unimelb.edu.au>
2020-01-20 19:19:35 +1100

    create bar graph and save figure

--- analysis_scripts/yh_lab_slackers.py
+++ analysis_scripts/yh_lab_slackers.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 
 input_path = "/Users/yoonheechoi/Desktop/Github repositories/lab_slackers/Raw_data/raw_data.csv"
 
@@ -12,4 +15,7 @@ df['Month'] = df['Date'].dt.month
 pd.DataFrame.groupby(df['Year'])
 
 sum_per_month = df.groupby(['Year','Month']).sum()['Messages posted'].reset_index()
-sum_per_month.reset_index()
+sum_per_month.reset_index(inplace=True)
+
+sns.barplot(x=sum_per_month['index'], y=sum_per_month['Messages posted'])
+plt.savefig('/Users/yoonheechoi/Desktop/Github repositories/lab_slackers/yh_plot.png')
