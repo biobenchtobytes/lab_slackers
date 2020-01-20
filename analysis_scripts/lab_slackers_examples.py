9fe6c3d81c680decce868bb5822485f821e88cd9
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2020-01-20 18:29:16 +1100

    New output path, Clean figures, add comments

--- analysis_scripts/lab_slackers_examples.py
+++ analysis_scripts/lab_slackers_examples.py
@@ -13,7 +13,7 @@ from GEN_Utils import FileHandling
 logger.info('Import OK')
 
 input_path = 'Raw_data/raw_data.csv'
-output_folder = 'Python_results'
+output_folder = 'dc_results/'
 
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
@@ -34,10 +34,7 @@ ProfileReport(summary_data)
 
 summary_data.columns.tolist()
 
-# number of private versus messages per day
-fig, ax = plt.subplots()
-sns.lineplot(x='Date', y='Messages posted', data=summary_data)
-
+# number of messages per day
 fig, ax = plt.subplots()
 cmap = sns.cubehelix_palette(light=1, as_cmap=True)
 sns.kdeplot(data=summary_data['day_of_week'].astype(
@@ -45,8 +42,9 @@ sns.kdeplot(data=summary_data['day_of_week'].astype(
 plt.xticks(ticks=np.arange(7), labels=[
            'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
 plt.ylim(-10, 50)
+plt.savefig(f'{output_folder}dc_messages_per_day.png')
 
-
+# Scatterplot of messages per day
 fig, ax = plt.subplots()
 cmap = sns.cubehelix_palette(light=1, as_cmap=True)
 sns.swarmplot(x=summary_data['day_of_week'].astype(
@@ -54,3 +52,5 @@ sns.swarmplot(x=summary_data['day_of_week'].astype(
 plt.xticks(ticks=np.arange(7), labels=[
            'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
 plt.ylim(-10, 50)
+
+
