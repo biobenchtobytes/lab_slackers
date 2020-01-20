96974a4927ee7cd5cfaf4c78e84285c7870049e9
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2020-01-20 17:33:44 +1100

    Adding example script

---
+++ analysis_scripts/lab_slackers_examples.py
@@ -0,0 +1,56 @@
+import os
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
+
+from pandas_profiling import ProfileReport
+from loguru import logger
+from GEN_Utils import FileHandling
+
+logger.info('Import OK')
+
+input_path = 'Raw_data/raw_data.csv'
+output_folder = 'Python_results'
+
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+
+raw_data = pd.read_csv(input_path)
+
+# convert date-time column
+summary_data = raw_data.copy()
+summary_data['Date'] = pd.to_datetime(
+    summary_data['Date'], infer_datetime_format=True)
+summary_data['day_of_week'] = summary_data['Date'].dt.dayofweek
+summary_data['total_messages'] = summary_data['Messages in public channels'] + \
+    summary_data['Messages in private channels'] + \
+    summary_data['Messages in shared channels'] + \
+    summary_data['Messages in DMs']
+summary_data.describe()
+ProfileReport(summary_data)
+
+summary_data.columns.tolist()
+
+# number of private versus messages per day
+fig, ax = plt.subplots()
+sns.lineplot(x='Date', y='Messages posted', data=summary_data)
+
+fig, ax = plt.subplots()
+cmap = sns.cubehelix_palette(light=1, as_cmap=True)
+sns.kdeplot(data=summary_data['day_of_week'].astype(
+    int), data2=summary_data['total_messages'].astype(int), shade=True, cmap=cmap,)
+plt.xticks(ticks=np.arange(7), labels=[
+           'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+plt.ylim(-10, 50)
+
+
+fig, ax = plt.subplots()
+cmap = sns.cubehelix_palette(light=1, as_cmap=True)
+sns.swarmplot(x=summary_data['day_of_week'].astype(
+    int), y=summary_data['total_messages'].astype(int))
+plt.xticks(ticks=np.arange(7), labels=[
+           'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+plt.ylim(-10, 50)
