88d8a6a18af2d27e4e2d0bf8107f217dd142c4c0
Chez-ter <60080816+Chez-ter@users.noreply.github.com>
2020-01-20 18:50:13 +1100

    Chez_1st_exercise

---
+++ analysis_scripts/test_script_Chez.R
@@ -0,0 +1,18 @@
+my_data = read.csv('raw_data.csv', header=TRUE, sep=",")
+head(my_data)
+library(ggplot2)
+my_data = my_data[1:3]
+my_data
+colnames(my_data) = c('A','B','C')
+my_plot = ggplot(my_data, aes(x = Daily.active.members, y = Messages.in.DMs)) + geom_point()
+my_plot
+ggsave('Chez_plot.png')
+ggplot
+
+
+x = c(-10:10)
+y = x^2
+df = data.frame(x,y)
+df
+plot = ggplot(df, aes(x=x,y=y))
+plot
