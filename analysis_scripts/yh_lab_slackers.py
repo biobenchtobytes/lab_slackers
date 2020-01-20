104992472d46062a98090a163a6517eb8c520f41
Yoon Hee Choi <yoonheec@student.unimelb.edu.au>
2020-01-20 18:34:22 +1100

    copy raw data and change date format

--- analysis_scripts/yh_lab_slackers.py
+++ analysis_scripts/yh_lab_slackers.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 input_path = "/Users/yoonheechoi/Desktop/Github repositories/lab_slackers/Raw_data/raw_data.csv"
 
-pd.read_csv(input_path)
+raw_data = pd.read_csv(input_path)
+
+df = pd.DataFrame.copy(raw_data)
+df['Date'] = pd.to_datetime(df['Date'], infer_datetime_format=True)
+df['day_of_week'] = df['Date'].dt.dayofweek
