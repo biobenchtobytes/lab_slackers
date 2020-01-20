977fc2f5118a16a4e38fadd3541eed2e5dd65db0
Yoon Hee Choi <yoonheec@student.unimelb.edu.au>
2020-01-20 19:01:35 +1100

    change datetime format, groupby month and year create new df with sum of messages posted

--- analysis_scripts/yh_lab_slackers.py
+++ analysis_scripts/yh_lab_slackers.py
@@ -6,4 +6,10 @@ raw_data = pd.read_csv(input_path)
 
 df = pd.DataFrame.copy(raw_data)
 df['Date'] = pd.to_datetime(df['Date'], infer_datetime_format=True)
-df['day_of_week'] = df['Date'].dt.dayofweek
+df['Year'] = df['Date'].dt.year
+df['Month'] = df['Date'].dt.month
+
+pd.DataFrame.groupby(df['Year'])
+
+sum_per_month = df.groupby(['Year','Month']).sum()['Messages posted'].reset_index()
+sum_per_month.reset_index()
